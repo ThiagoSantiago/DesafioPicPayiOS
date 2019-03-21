f0383154fbbbb92970fea013610e80187569ceb2
Thiago Santiago <thiagoalexandres@gmail.com>
2019-03-21 14:53:04 -0300

    Add tests for the AppRouter.

--- DesafioPicPayiOS/DesafioPicPayiOSTests/Tests/AppRouterTests.swift
+++ DesafioPicPayiOS/DesafioPicPayiOSTests/Tests/AppRouterTests.swift
@@ -27,4 +27,56 @@ class AppRouterTests: XCTestCase {
             router.navigation.viewControllers.first!.isKind(of: ContactsListViewController.self)
         )
     }
+    
+    func testWhenCallRouteToNewCardNavigationPushNewCardViewController() {
+        let viewModel = UserViewModel(id: 1001, name: "Eduardo Santos", img: "https://randomuser.me/api/portraits/men/9.jpg", username: "@eduardo.santos")
+        
+        router.routeToNewCard(user: viewModel)
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: NewCardViewController.self)
+        )
+    }
+    
+    func testWhenCallRouteToRegisterNewCardNavigationPushRegisterNewCardViewController() {
+        let user = UserViewModel(id: 1002, name: "Marina Coelho", img: "https://randomuser.me/api/portraits/women/37.jpg", username: "@marina.coelho")
+        
+        let card = Card(owner: "1002", cardNumber: "1234123412341234", securityCode: "789", expirationDate: "01/18")
+        let paymentViewModel =  PaymentViewModel(card: card, value: "799", destinationUser: user)
+        
+        router.routeToRegisterNewCard(viewModel: paymentViewModel)
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: RegisterNewCardViewController.self)
+        )
+    }
+    
+    func testWhenCallRouteToPaymentNavigationPushPaymentViewController() {
+        let user = UserViewModel(id: 1002, name: "Marina Coelho", img: "https://randomuser.me/api/portraits/women/37.jpg", username: "@marina.coelho")
+        
+        let card = Card(owner: "1002", cardNumber: "1234123412341234", securityCode: "789", expirationDate: "01/18")
+        let paymentViewModel =  PaymentViewModel(card: card, value: "799", destinationUser: user)
+        
+        router.routeToPayment(paymentViewModel)
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: PaymentViewController.self)
+        )
+    }
+    
+    func testWhenCallPopViewControllerThenNavigationPopTheTopViewController() {
+         let viewModel = UserViewModel(id: 1001, name: "Eduardo Santos", img: "https://randomuser.me/api/portraits/men/9.jpg", username: "@eduardo.santos")
+        
+        router.routeToHome()
+        router.routeToNewCard(user: viewModel)
+        router.popViewController()
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: ContactsListViewController.self)
+        )
+    }
 }
